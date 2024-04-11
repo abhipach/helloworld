667ebcbc9c3920284c243a7071ed1c0755f20a47
ABHISHEK PACHAURI <40334999+abhipach@users.noreply.github.com>
2024-04-11 16:02:38 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,16 @@
-resource "google_storage_bucket" "auto-expire" {
-  name          = "auto-expiring-bucket"
-  location      = "US"
-  force_destroy = true
+resource "google_spanner_instance" "main" {
+  config       = "regional-europe-west3"
+  display_name = "testdemo"
+  num_nodes    = 1
+}
 
-  lifecycle_rule {
-    condition {
-      age = 3
-    }
-    action {
-      type = "Delete"
-    }
-  }
-
-  lifecycle_rule {
-    condition {
-      age = 1
-    }
-    action {
-      type = "AbortIncompleteMultipartUpload"
-    }
-  }
+resource "google_spanner_database" "database" {
+  instance = google_spanner_instance.main.name
+  name     = "my-database"
+  version_retention_period = "3d"
+  ddl = [
+    "CREATE TABLE t1 (t1 INT64 NOT NULL,) PRIMARY KEY(t1)",
+    "CREATE TABLE t2 (t2 INT64 NOT NULL,) PRIMARY KEY(t2)",
+  ]
+  deletion_protection = false
 }
