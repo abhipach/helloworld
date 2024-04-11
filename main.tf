9a49386f0ae50a73656ee60434a25dc85b066ec2
ABHISHEK PACHAURI <40334999+abhipach@users.noreply.github.com>
2024-04-11 16:09:55 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@ resource "google_spanner_instance" "main" {
 }
 
 resource "google_spanner_database" "database" {
-  instance = google_spanner_instance.main.name
-  name     = "my-database"
+  instance                 = google_spanner_instance.main.name
+  name                     = "my-database"
   version_retention_period = "3d"
   ddl = [
     "CREATE TABLE t1 (t1 INT64 NOT NULL,) PRIMARY KEY(t1)",
